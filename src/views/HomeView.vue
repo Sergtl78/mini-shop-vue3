64f7746d49aaa20a51657c4fbbb2e54a6386bbf1
Sergey <sergtl78@gmail.com>
2024-10-15 12:07:31 +0300

    change origin

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <main class="container">
+    This is Home page
+    <Button>Click</Button>
+  </main>
+</template>
